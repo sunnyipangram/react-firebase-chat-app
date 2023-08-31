8cc79e90df132c39815278b0fc316d74e09062bd
Sunny Jadaun <sunnyjadaun@ipangram.com>
2023-08-31 17:50:14 +0530

    Added Notification Functionality

--- src/Components/Auth.js
+++ src/Components/Auth.js
@@ -1,35 +1,33 @@
-import React from 'react'
-import { auth,provider } from '../FirebaseConfig'
-import {signInWithPopup} from 'firebase/auth'
+import { signInWithPopup } from 'firebase/auth';
+import { auth, provider, db } from '../FirebaseConfig';
+import { doc, setDoc } from 'firebase/firestore';
+import { getMessaging,getToken } from 'firebase/messaging';
 import Cookies from 'universal-cookie';
-const cookies= new Cookies()
+
+const cookies = new Cookies();
+
 const Auth = (props) => {
-    const {setIsAuth}=props
+  const { setIsAuth } = props;
 
+  const SignInWithGoogle = async () => {
+    try {
+      let result = await signInWithPopup(auth, provider);
+      cookies.set('auth-token', result.user.refreshToken);
+      setIsAuth(true);
 
-
-
-
-    const SignInWithGoogle=async(props)=>{
-
-
-        try{
-            let result= await signInWithPopup(auth,provider)
-            cookies.set('auth-token',result.user.refreshToken)
-            setIsAuth(true)
-             console.log(result)
-        }
-        catch(err){
-            console.log(err)
-        }
-
+     
+      console.log(result);
+    } catch (err) {
+      console.log(err);
     }
+  };
+
   return (
     <div className='auth'>
-        <p>Sign In With Google To Continue</p>
-        <button onClick={SignInWithGoogle}>Sign In WIth Google</button>
+      <p>Sign In With Google To Continue</p>
+      <button onClick={SignInWithGoogle}>Sign In With Google</button>
     </div>
-  )
-}
+  );
+};
 
-export default Auth
+export default Auth;
--- src/Components/Chat.js
+++ src/Components/Chat.js
@@ -3,23 +3,59 @@ import React, { useEffect, useState } from 'react';
 import { auth, db, messaging } from '../FirebaseConfig';
 import { addDoc, collection, serverTimestamp, query, where, orderBy, onSnapshot } from 'firebase/firestore';
 import '../Style/Chat.css';
-
+import { getMessaging, getToken } from 'firebase/messaging';
+import Sidebar from './Sidebar';
+import {BiSolidSend} from 'react-icons/bi'
 const Chat = (props) => {
   const { Room } = props;
   const [newMessage, setNewMessage] = useState('');
   const [messages, setMessages] = useState([]);
+  const messaging = getMessaging(); // Initialize the messaging instance
+  const [lastNotificationTime, setLastNotificationTime] = useState(null);
+
 
   useEffect(() => {
+    // Request notification permission
     const requestNotificationPermission = async () => {
-      const permission = await Notification.requestPermission();
-      console.log('Notification permission:', permission);
+      try {
+        await messaging.requestPermission();
+        const token = await getToken(messaging);
+        console.log('FCM Token:', token);
+      } catch (error) {
+        console.error('Notification permission error:', error);
+      }
     };
-  
+
     requestNotificationPermission();
   }, []);
   
 
   useEffect(() => {
+  
+
+    const handleNewMessage = async (message) => {
+      const isCurrentUserMessage = message.user === auth.currentUser.displayName;
+
+      if (!isCurrentUserMessage && message.createdAt) {
+        const messageTimestamp = message.createdAt.toMillis();
+        const currentTime = Date.now();
+
+        const timeDifference = currentTime - messageTimestamp;
+        const withinLast10Seconds = timeDifference <= 10000;
+
+        if (withinLast10Seconds && lastNotificationTime !== messageTimestamp) {
+          // Check if a notification hasn't already been sent for this message
+          setLastNotificationTime(messageTimestamp);
+
+          const notificationOptions = {
+            body: message.text,
+          };
+
+          new Notification(message.user, notificationOptions);
+        }
+      }
+    };
+
     const messagesRef = collection(db, 'messages');
     const queryMessages = query(messagesRef, where('room', '==', Room), orderBy('createdAt'));
 
@@ -29,6 +65,11 @@ const Chat = (props) => {
         messages.push({ ...doc.data(), id: doc.id });
       });
       setMessages(messages);
+
+      if (messages.length > 0) {
+        const latestMessage = messages[messages.length - 1];
+        handleNewMessage(latestMessage);
+      }
     });
 
     return () => unsubscribe();
@@ -58,24 +99,37 @@ const Chat = (props) => {
       createdAt: serverTimestamp(),
       user: auth.currentUser.displayName,
       room: Room,
+      profileUrl: auth.currentUser.photoURL
     });
     setNewMessage('');
+
+    // Send a notification after sending a message
+    // const notification = new Notification(auth.currentUser.displayName, {
+    //   body: newMessage,
+    // });
   };
   console.log(auth.currentUser.photoURL)
 
   return (
     <div className="chat-app">
+      <Sidebar/>
       <div className="header">
         <h1>Welcome To {Room.toUpperCase()}</h1>
       </div>
       <div className="message-box">
-        {messages.map((message) => (
-          <div className={`message ${message.user === auth.currentUser.displayName ? 'left' : 'right'}`} key={message.id}>
-           <span className="user">{message.user}</span>
-            {message.text}
-          </div>
-        ))}
-      </div>
+    {messages.map((message) => {
+      const isCurrentUserMessage = message.user === auth.currentUser.displayName;
+
+    
+      return (
+        <div className={`message ${isCurrentUserMessage ? 'left' : 'right'}`} key={message.id} style={{display:'flex',alignItems:'center',gap:'10px'}}>
+       <img src={!message.profileUrl? "https://www.kindpng.com/picc/m/252-2524695_dummy-profile-image-jpg-hd-png-download.png":  message.profileUrl} style={{height:'25px',width:'25px',borderRadius:'50%'}} alt="" />   <span className="user">{message.user}</span>
+          {message.text}
+        </div>
+      );
+    })}
+  </div>
+
       <form className="new-message-form" onSubmit={handleSubmit}>
         <input
           type="text"
@@ -84,8 +138,8 @@ const Chat = (props) => {
           value={newMessage}
           onChange={(e) => setNewMessage(e.target.value)}
         />
-        <button type="submit" className="send-button">
-          Send Message
+        <button type="submit" className="send-button" style={{display:'flex',alignItems:'center',gap:'5px'}}>
+          Send<BiSolidSend/>
         </button>
       </form>
     </div>
@@ -116,93 +170,3 @@ export default Chat;
 
 
 
-
-
-// import React, { useEffect, useState } from 'react';
-// import { auth, db, messaging } from '../FirebaseConfig'; // Make sure to import the messaging instance
-// import { addDoc, collection, serverTimestamp, query, where, orderBy, onSnapshot } from 'firebase/firestore';
-// import '../Style/Chat.css';
-
-// const Chat = (props) => {
-//   const { Room } = props;
-//   const [newMessage, setNewMessage] = useState('');
-//   const [messages, setMessages] = useState([]);
-
-//   useEffect(() => {
-//     const messagesRef = collection(db, 'messages');
-//     const queryMessages = query(messagesRef, where('room', '==', Room), orderBy('createdAt'));
-
-//     const unsubscribe = onSnapshot(queryMessages, (snapshot) => {
-//       let messages = [];
-//       snapshot.forEach((doc) => {
-//         messages.push({ ...doc.data(), id: doc.id });
-//       });
-//       setMessages(messages);
-//     });
-
-//     return () => unsubscribe();
-//   }, [Room]);
-
-//   useEffect(() => {
-//     // Request permission and get registration token for notifications
-//     const requestNotificationPermission = async () => {
-//       try {
-//         await Notification.requestPermission();
-//         const token = await messaging.getToken();
-//         console.log('FCM Token:', token);
-
-//         // Save the token in your database (Firestore) for sending targeted notifications
-//         // For example, you can save it in the user's document
-//         // const userDocRef = doc(db, 'users', auth.currentUser.uid);
-//         // await updateDoc(userDocRef, { fcmToken: token });
-//       } catch (error) {
-//         console.error('Notification permission error:', error);
-//       }
-//     };
-
-//     requestNotificationPermission();
-//   }, []);
-
-//   const handleSubmit = async (e) => {
-//     e.preventDefault();
-//     if (newMessage === '') return;
-
-//     await addDoc(collection(db, 'messages'), {
-//       text: newMessage,
-//       createdAt: serverTimestamp(),
-//       user: auth.currentUser.displayName,
-//       room: Room,
-//     });
-//     setNewMessage('');
-//   };
-
-//   return (
-//     <div className="chat-app">
-//       <div className="header">
-//         <h1>Welcome To {Room.toUpperCase()}</h1>
-//       </div>
-//       <div className="message-box">
-//         {messages.map((message) => (
-//           <div className={`message ${message.user === auth.currentUser.displayName ? 'left' : 'right'}`} key={message.id}>
-//             <span className="user">{message.user}</span>
-//             {message.text}
-//           </div>
-//         ))}
-//       </div>
-//       <form className="new-message-form" onSubmit={handleSubmit}>
-//         <input
-//           type="text"
-//           className="new-message-input"
-//           placeholder="Type your message here"
-//           value={newMessage}
-//           onChange={(e) => setNewMessage(e.target.value)}
-//         />
-//         <button type="submit" className="send-button">
-//           Send Message
-//         </button>
-//       </form>
-//     </div>
-//   );
-// };
-
-// export default Chat;
